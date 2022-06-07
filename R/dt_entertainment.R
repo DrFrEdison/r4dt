3378f15b168a2f60dc611896275d0cf17b4ac4ac
DrFrEdison <kurtz@mailbox.org>
2022-06-07 14:13:37 +0200

    remove/adapt data for package building

--- R/dt_entertainment.R
+++ R/dt_entertainment.R
@@ -1,18 +1,18 @@
 # Entertainment ####
-if( length(suppressWarnings(findWavPlayer())) == 0) setWavPlayer("C:/Users/Public/wav_play/wv_player.exe")
+# if( length(suppressWarnings(findWavPlayer())) == 0) setWavPlayer("C:/Users/Public/wav_play/wv_player.exe")
 
-.entertainment <- list()
-.entertainment$alarm <- load.wave(paste0(wd$entertain,"sound/alarm.wav"))
-.entertainment$egal <- load.wave(paste0(wd$entertain,"sound/egal.wav"))
-.entertainment$egal_image <- load.image(paste0(wd$entertain,"pics/egal.png"))
-.entertainment$alarm_image <- load.image(paste0(wd$entertain,"pics/alarm.png"))
+# .entertainment <- list()
+# .entertainment$alarm <- load.wave(paste0(wd$entertain,"sound/alarm.wav"))
+# .entertainment$egal <- load.wave(paste0(wd$entertain,"sound/egal.wav"))
+#.entertainment$egal_image <- load.image(paste0(wd$entertain,"pics/egal.png"))
+# .entertainment$alarm_image <- load.image(paste0(wd$entertain,"pics/alarm.png"))
 
-.entertainment$vfl <- load.wave(paste0(wd$entertain,"sound/11.wav"))
-.entertainment$drum <- load.wave(paste0(wd$entertain,"sound/drum_roll.wav"))
+# .entertainment$vfl <- load.wave(paste0(wd$entertain,"sound/11.wav"))
+# .entertainment$drum <- load.wave(paste0(wd$entertain,"sound/drum_roll.wav"))
 
-.breakfun <- function(x)
-{
-  #p1 <- proc.time()
-  Sys.sleep(x)
-  #proc.time() - p1 # The cpu usage should be negligible
-}
+# .breakfun <- function(x)
+# {
+#   #p1 <- proc.time()
+#   Sys.sleep(x)
+#  #proc.time() - p1 # The cpu usage should be negligible
+# }
--- R/source_pca.R
+++
@@ -1,4 +0,0 @@
-source( paste0( dt$R, "R/wd.R" ) )
-source( paste0( wd$scripts, "packages_pca.R" ) )
-source( paste0( wd$scripts, "_function_global.R" ) )
-source( paste0( wd$scripts, "_function_pca_plotpara.R" ) )
