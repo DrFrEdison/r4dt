4f23328d811a087a4218593fc339d8abb656b076
DrFrEdison <kurtz@mailbox.org>
2022-06-08 10:03:10 +0200

    add data to r4dt

--- data.R
+++ data.R
@@ -1,10 +1,7 @@
-usethis::use_git()
-usethis::use_github()
-
 dir(wd$data)
 dt_customer <- fread( paste0(wd$data, "/dt_customer.csv"))
 dt_customer_product_ID <- fread( paste0(wd$data, "/dt_customer_product_ID.csv"))
 
 setwd("D:/R_Git/r4dt/data")
 save(dt_customer, file =  "dt_customer.rda")
-save(dt_customer_product_ID, file =  "dt_customer.rda")
+save(dt_customer_product_ID, file =  "dt_customer_product_ID.rda")
