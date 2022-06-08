fda12e9e6b778ce20ac2abfcec9a1df334a62e8a
DrFrEdison <kurtz@mailbox.org>
2022-06-08 10:15:29 +0200

    add data.R in ~/R

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Customers, Locations, Lines and LG's
+#'
+#' A dataset containing all customers
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{customer}{ lala}
+#'   \item{line}{ lala }
+#'   ...
+#' }
+#' @source \url{own}
+"dt_customer"
