5c5ba99bd1c54512bb066ea55b72185d1a87e471
DrFrEdison <kurtz@mailbox.org>
2022-06-08 10:28:31 +0200

    data correction

--- R/data.R
+++ R/data.R
@@ -1,6 +1,10 @@
+#' dt_customer.
 #' Customers, Locations, Lines and LG's
 #'
 #' A dataset containing all customers
+#'
+#' @docType data
+#' @usage data(dt_customer)
 #'
 #' @format A data frame
 #' \describe{
@@ -10,3 +14,20 @@
 #' }
 #' @source \url{own}
 "dt_customer"
+
+#' dt_customer_product_ID
+#' Customers, Locations, Lines and LG's
+#'
+#' A dataset containing all customers
+#'
+#' @docType data
+#' @usage data(dt_customer_product_ID)
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{customer}{ lala}
+#'   \item{line}{ lala }
+#'   ...
+#' }
+#' @source \url{own}
+"dt_customer_product_ID"
