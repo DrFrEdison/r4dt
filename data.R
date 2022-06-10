37510f31aba65f3b6235655acffac6094c5c790b
MPK <kurtz@mailbox.org>
2022-06-10 14:33:49 +0200

    update data

--- data.R
+++ data.R
@@ -1,7 +1,10 @@
+library(r4dt)
 dir(wd$data)
 dt_customer <- fread( paste0(wd$data, "/dt_customer.csv"))
 dt_customer_product_ID <- fread( paste0(wd$data, "/dt_customer_product_ID.csv"))
 
-setwd("D:/R_Git/r4dt/data")
+
+setwd(this.path::this.dir())
+setwd("./data")
 save(dt_customer, file =  "dt_customer.rda")
 save(dt_customer_product_ID, file =  "dt_customer_product_ID.rda")
