c9c7a9415dead86b85d347dcd05cf764e2225942
MPK <kurtz@mailbox.org>
2022-06-24 15:36:45 +0200

    add function ma.date

--- R/global.R
+++ R/global.R
@@ -12,6 +12,28 @@ parusr <- function(position, xx = .15, yy = .2){
 
 ma <- function(x, n = 5){stats::filter(x, rep(1 / n, n), sides = 1)}
 
+ma.date <- function(x, time, diff.time.max = 1200, n = 5){
+
+  diff.time <- diff(time)
+  diff.time.seq <- which(diff.time > diff.time.max)
+  x.ma <- x
+
+  for(i in 1:length(diff.time.seq)){
+
+    if(i == 1)  x.ma[  1:diff.time.seq[i] ] <- as.numeric( ma( dat1$co2[ 1:diff.time.seq[i] ],
+                                                               ifelse(length( 1:diff.time.seq[i] ) > n, n, length(( 1:diff.time.seq[i] ))) ))
+
+    if(i > 1 & i < length(diff.time.seq)){
+      x.ma[  (diff.time.seq[ i-1 ] + 1) :diff.time.seq[i] ] <- as.numeric( ma( dat1$co2[ (diff.time.seq[ i-1 ] + 1) :diff.time.seq[i] ]
+                                                                               , ifelse(length((diff.time.seq[ i-1 ] + 1) :diff.time.seq[i] ) > n, n, length((diff.time.seq[ i-1 ] + 1) :diff.time.seq[i] ))) )
+    }
+
+    if(i == length(diff.time.seq)) x.ma[  (diff.time.seq[ i ] + 1) : diff.time.seq[ i ] ] <- as.numeric( ma( dat1$co2[ (diff.time.seq[ i ] + 1) : diff.time.seq[ i ] ],
+                                                                                                             ifelse( length((diff.time.seq[ i ] + 1) : diff.time.seq[ i ]) > n, n, length((diff.time.seq[ i ] + 1) : diff.time.seq[ i ]))) )
+  }
+
+  return(x.ma)
+}
 
 moving.average.time <- function(ma.dat, ma.time, ma.minutes = 10, ma.n = 5){
 
