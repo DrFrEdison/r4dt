1f0b956aad77a2839499e4a76d1aafd7373881bd
MPK <kurtz@mailbox.org>
2022-06-29 09:40:19 +0200

    is wl3 NA | 0

--- R/model_keepout.R
+++ R/model_keepout.R
@@ -13,20 +13,21 @@ keep.out.unsb <- function(model, wl1, wl2, wl3, wl4){
     keepout.1 = max(which(model$wl < wl2)) + ncolp
   }
 
-  if(!is.na(wl3)){
+  suppressMessages(ifelse(all(!is.na(wl3), wl3 != 0), ttt <- T, ttt <- F))
+  if(ttt){
     keepout.3 <- min(which(model$wl > wl2 & model$wl < wl3)) + ncolp
     keepout.4 <- max(which(model$wl > wl2 & model$wl < wl3)) + ncolp
 
     keepout.5 <- min(which(model$wl > wl4)) + ncolp
   }
 
-  if(is.na(wl3)){
+  if(ttt){
     keepout.5 <- min(which(model$wl > wl2)) + ncolp
   }
 
-  if(!is.na(wl3) & wl1 != min(model$wl)) keepout.return <- paste0(keepout.1, "-", keepout.2, ",", keepout.3, "-", keepout.4, ",", keepout.5, "-", keepout.max)
-  if(is.na(wl3) & wl1 != min(model$wl)) keepout.return <- paste0(keepout.1, "-", keepout.2, ",", keepout.5, "-", keepout.max)
-  if(is.na(wl3) & wl1 == min(model$wl)) keepout.return <- paste0(keepout.1, "-", keepout.max)
+  if(ttt & wl1 != min(model$wl)) keepout.return <- paste0(keepout.1, "-", keepout.2, ",", keepout.3, "-", keepout.4, ",", keepout.5, "-", keepout.max)
+  if(!ttt & wl1 != min(model$wl)) keepout.return <- paste0(keepout.1, "-", keepout.2, ",", keepout.5, "-", keepout.max)
+  if(!ttt & wl1 == min(model$wl)) keepout.return <- paste0(keepout.1, "-", keepout.max)
 
   return(keepout.return)
 }
