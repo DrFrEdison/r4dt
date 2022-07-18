daa3f5b7383a30266c96572c48c63cc78e92cf95
MPK <kurtz@mailbox.org>
2022-07-18 15:17:22 +0200

    read_log and drop modified for spc, drk, ref

--- R/read_log_drop.R
+++ R/read_log_drop.R
@@ -1,12 +1,12 @@
-drop_log <- function(path = dirname(rstudioapi::getSourceEditorContext()$path), recursive = T){
+drop_log1 <- function(path = dirname(rstudioapi::getSourceEditorContext()$path), recursive = T, type = "spc", SG = F){
   writelog <- list()
-  
-  writelog$spc <- dir( path = path, pattern = "M.log")
-  writelog$ref <- dir( path = path, pattern = "R.log")
-  writelog$drk <- dir( path = path, pattern = "D.log")
-  
+
+  if( type == "spc")  writelog$spc <- dir( path = path, pattern = "M.log")
+  if( type == "ref")  writelog$spc <- dir( path = path, pattern = "R.log")
+  if( type == "drk")  writelog$spc <- dir( path = path, pattern = "D.log")
+
   setwd(path)
-  
-  writelog$read_spc <-  read_log_files(logfiles = writelog$spc
-                                       , SG = SG)
+
+  read_log_files(logfiles = writelog$spc, SG = SG, type = type)
+
 }
